5439000de338eec7aaac6fc5bc45a7572506d4d3
Amy Kotas <aekotas@gmail.com>
2017-02-08 06:22:02 -0500

    Went through site and fixed JS problem that was breaking responsive navbar. 🙂

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
-(function($) {
-  $(function() {
-    $('nav ul li > a:not(:only-child)').click(function(e) {
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
       $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
       $('.nav-dropdown').not($(this).siblings()).hide();
       e.stopPropagation();
     });
+    // Clicking away from dropdown will remove the dropdown class
     $('html').click(function() {
       $('.nav-dropdown').hide();
     });
-  });
-})(jQuery);
-
-// Toggle the .active span.
-
-$('#nav-toggle').on('click', function() {
-  this.classList.toggle('active');
-});
-
-// Toggle the nav ul on click.
-
-$('#nav-toggle').click(function() {
-  $('nav ul').toggle();
-});
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
